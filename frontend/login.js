213aeb9262c5468f38a6ebc590aca9d9e27b82e9
Jababean <71944013+Jababean@users.noreply.github.com>
2022-10-15 16:39:21 -0700

    test

--- frontend/login.js
+++ frontend/login.js
@@ -1,18 +1,14 @@
-const uploadCodeFile = () => {
+const registration = async () => {
+    
+    let name = document.getElementById("name").value;
     let cpu = document.getElementById("cpu").value;
     let gpu = document.getElementById("gpu").value;
     let os = document.getElementById("os").value;
     let ram = document.getElementById("ram").value;
     let lang = document.getElementById("lang").value.split(",");
-
-    const user = {
-      CPU:cpu,
-      GPU:gpu,
-      OS:os,
-      RAM:ram,
-      languages:lang,
-    }
-    console.log(user);
+    
+    let uid = await registerAccount(name, cpu, gpu, os, ram, lang);
+    localStorage.setItem("uid", uid);
 }
 
-document.getElementById("login-form-submit").addEventListener("click", uploadCodeFile);
+document.getElementById("login-form-submit").addEventListener("click", registration);
