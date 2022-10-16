f93a474ee0908c9d92c634e976c8cd0187e77728
Adam Podoxin <adam.podoxin@gmail.com>
2022-10-15 22:08:57 -0700

    Move provider code to compile.html/js

---
+++ frontend/compile.js
@@ -0,0 +1,40 @@
+const uid = localStorage.getItem("uid");
+const toClientUID = localStorage.getItem("to_client_uid");
+
+function forceDownload(blob, filename) {
+	var a = document.createElement("a");
+	a.download = filename;
+	a.href = blob;
+	document.body.appendChild(a);
+	a.click();
+	a.remove();
+}
+
+function downloadResource(url, filename) {
+	if (!filename) filename = url.split("\\").pop().split("/").pop();
+	fetch(url, {
+		headers: new Headers({
+			Origin: location.origin,
+		}),
+		mode: "cors",
+	})
+		.then((response) => response.blob())
+		.then((blob) => {
+			let blobUrl = window.URL.createObjectURL(blob);
+			forceDownload(blobUrl, filename);
+		})
+		.catch((e) => console.error(e));
+}
+
+getCodeFileDownload(toClientUID, uid).then((data) => {
+	console.log(data.instructions);
+	document.getElementById("instructions").innerHTML += data.instructions;
+	downloadResource(data.url, data.fileName);
+});
+
+const upload = async () => {
+	const selectedFile = document.getElementById("file").files[0];
+
+	const url = await sendCompiledFile(uid, toClientUID, selectedFile);
+	await sendCompiledMessage(uid, toClientUID, url);
+};
