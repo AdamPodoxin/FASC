04b2a4c01d439f9e1bd5e18d26e82035245c7b88
Adam Podoxin <adam.podoxin@gmail.com>
2022-10-15 22:32:58 -0700

    Redirect to home page after request

--- frontend/request.js
+++ frontend/request.js
@@ -5,7 +5,9 @@ const upload = async () => {
 	const selectedFile = document.getElementById("file").files[0];
 	const fileName = selectedFile.name;
 	const instructions = document.getElementById("instructions").value;
-	
+
 	const url = await sendCodeFile(uid, toProviderUID, selectedFile);
 	await sendCompileRequest(uid, toProviderUID, url, fileName, instructions);
+
+	window.location = "./client.html";
 };
