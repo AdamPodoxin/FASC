39cbdad4c0f20d230543a90c3643e74fcffda736
Adam Podoxin <31609249+AdamPodoxin@users.noreply.github.com>
2022-10-15 17:27:36 -0700

    Make request page send request properly

--- backend/api.js
+++ backend/api.js
@@ -16,6 +16,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
 // https://stackoverflow.com/a/6248722
 const generateUID = () => {
