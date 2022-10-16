89006748c03147a582325fef2fefbe3639c0d2ef
Adam Podoxin <31609249+AdamPodoxin@users.noreply.github.com>
2022-10-15 17:22:07 -0700

    Delete request from wrong folder

--- Frontend/request.js
+++
@@ -1,8 +0,0 @@
-const uid = localStorage.getItem("uid");
-const toProviderUID = localStorage.getItem("to_provider_uid");
-
-const upload = () => {
-	const selectedFile = document.getElementById("file").files[0];
-	const instructions = document.getElementById("instructions").value;
-	console.log(instructions);
-};
